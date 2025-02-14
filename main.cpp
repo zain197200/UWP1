039a47a7b15b96848a6e35850eeccc6ada0e8732
zain197200 <Zainnoor.pk.123@gmail.com>
2025-02-14 11:59:00 +0000

    add things

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@
 int length;
 char buffer[BUFF_LENGTH] = {0};
 BufferedSerial pc(USBTX, USBRX, 9600);
+char buf[32] = {0};
+char MSG[32];
+int length;
+
+if (uint32_t num = pc.read(buf, sizeof(buf))) {
+// String compare buf with the character 1. Equals 0 if match.
+if(strcmp(buf, "1") == 0)
+{
+led = !led;
+length = snprintf(MSG,MSG_BUFF_LENGTH, "Received Command LED1\r\n");
+pc.write(MSG,length);
+}
+}
+
 // main() runs in its own thread in the OS
 int main()
 {
